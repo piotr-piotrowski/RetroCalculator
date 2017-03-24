52286f357427f1ad625f0713f2a2657f21f836c9
Piotr Piotrowski <piotr@piotrovski.com>
2017-03-24 10:46:38 +0100

    running calculator

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -13,6 +13,21 @@ class ViewController: UIViewController {
 
     var btnSound: AVAudioPlayer!
     
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    
+    var currentOperation = Operation.Empty
+    var runnningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var result = ""
+
+    @IBOutlet weak var outputLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,10 +40,35 @@ class ViewController: UIViewController {
         } catch let err as NSError {
             print(err.debugDescription)
         }
+        
+        outputLbl.text = ""
     }
     
     @IBAction func numberPressed(sender: UIButton) {
         playSound()
+        
+        runnningNumber += "\(sender.tag)"
+        outputLbl.text = runnningNumber
+    }
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(operation: .Divide)
+    }
+
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(operation: .Multiply)
+    }
+    
+    @IBAction func onSubractPressed(sender: AnyObject) {
+        processOperation(operation: .Subtract)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(operation: .Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(operation: currentOperation)
     }
 
     func playSound() {
@@ -36,6 +76,36 @@ class ViewController: UIViewController {
             btnSound.stop()
         }
         btnSound.play()
+    }
+    
+    func processOperation(operation: Operation) {
+        playSound()
+        if currentOperation != Operation.Empty {
+            // A user selected an operator, but then selected another operator without entering first a number
+            if runnningNumber != "" {
+                rightValStr = runnningNumber
+                runnningNumber = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                } else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                } else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                } else if currentOperation == Operation.Add {
+                    result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                }
+                
+                leftValStr = result
+                outputLbl.text = result
+            }
+            currentOperation = operation
+        } else {
+            // This is the first time an operator has been pressed
+            leftValStr = runnningNumber
+            runnningNumber = ""
+            currentOperation = operation
+        }
     }
     
     override func didReceiveMemoryWarning() {
